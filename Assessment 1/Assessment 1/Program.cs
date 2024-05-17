243fca2a65d41b168f40cfcb69344b24aa9b6a3f
MMallory17 <mdmallory87@gmail.com>
2024-05-16 20:34:04 -0400

    First commit

---
+++ Assessment 1/Assessment 1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+using static System.Formats.Asn1.AsnWriter;
+
+double grade = double.Parse(Console.ReadLine());
+static bool IsPassing(double grade)
+{
+    if (grade > 65)
+    {
+        return true;
+    }
+    else if (grade <= 65)
+    {
+        return false;
+    }
+    Console.WriteLine(IsPassing(grade));
+}
+
+
+static double AverageGrades(double grade1, double grade2, double grade3)
+{
+    double total = (grade1 + grade2 + grade3) / 3;
+    
+}
+
+static string OddOrEvenPassing(double grade)
+{
+    if (IsPassing(grade) && grade % 2 != 0)
+    {
+        return "passing and odd";
+    }
+    else if (!IsPassing(grade) && grade % 2 != 0)
+    {
+        return "failing and odd";
+    }
+    else if (IsPassing(grade) && grade % 2 == 0)
+    {
+        return "passing and even";
+    }
+    else if (!IsPassing(grade) && grade % 2 == 0)
+    {
+        return "failing and even";
+    }
+    
+}
